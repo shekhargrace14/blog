46fcf552ca8a0e2e6b6c0c6abd5b7c6dabe706b7
shekhargrace14 <shekhargrace14@gmail.com>
2024-07-20 23:01:28 +0530

    ui changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,9 +10,9 @@ const Home = () => {
   return (
     <div>
       <Banner title={"Welcome to Blog"} paragraph={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam culpa quia, in a ratione laborum voluptatibus tempora quae corrupti incidunt aut sunt nam, reiciendis qui vitae officiis non vero dolorem."} button={"Learn More"} />
-      <main className='flex lg:container mx-auto my-4 gap-4'>
-        <section className='w-8/12'>
-          <div className='grid grid-cols-3 sm:grid-cols-2 gap-4'>
+      <main className='lg:flex lg:container mx-auto my-4 gap-4'>
+        <section className='lg:w-8/12 w-full'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
             {products.slice(0,9).map(item => (
               <div className='bg-[#f7f7f7]  border-gray-100 border-2 p-4' key={item.id}>
                 <iframe width="100%" height="auto" src={item.video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -29,7 +29,7 @@ const Home = () => {
           </div>
 
         </section>
-        <aside className='w-4/12 bg-gray-100'>
+        <aside className='lg:w-4/12 w-full bg-gray-100'>
 
         </aside>
       </main>
